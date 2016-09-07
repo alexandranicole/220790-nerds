e761f5fbdc648e083057a0515e8422bdb1d72a29
alexandranicole <alexandrochkab@rambler.ru>
2016-09-07 18:48:39 +0200

    проверка

--- js/script.js
+++ js/script.js
@@ -5,11 +5,9 @@ var form = popup.querySelector("form");
 var login = popup.querySelector("[name=write-name]");
 var email = popup.querySelector("[name=e-mail]");
 var storage = localStorage.getItem("login");
-
 button.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("feedback-content-show");
-
     if (storage) {
         login.value = storage;
         password.focus();
@@ -17,17 +15,12 @@ button.addEventListener("click", function(event) {
         login.focus();
     }
 });
-
 close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("feedback-content-show");
     popup.classList.remove("feedback-error");
-
-
 });
-
 form.addEventListener("submit", function(event) {
-
     if (!login.value || !email.value) {
         event.preventDefault();
         popup.classList.remove("feedback-error");
@@ -37,7 +30,6 @@ form.addEventListener("submit", function(event) {
         localStorage.setItem("login", login.value);
     }
 });
-
 window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
         if (popup.classList.contains("feedback-content-show")) {
